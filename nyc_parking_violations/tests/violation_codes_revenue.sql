c933340a2e71eaaef2df6f0c1783d29ee658b79f
Dmitry Orekhov <dm.orekhov@gmail.com>
2025-05-08 15:49:16 -0400

    Chapter 10-Testing Singular test

---
+++ nyc_parking_violations/tests/violation_codes_revenue.sql
@@ -0,0 +1,10 @@
+{{ config(severity = 'warn') }}
+SELECT
+    violation_code,
+    SUM(fee_usd) as total_revenue_usd
+FROM
+    {{ref('silver_parking_violation_codes')}}
+GROUP BY
+    violation_code
+HAVING
+    NOT(total_revenue_usd >= 1)
